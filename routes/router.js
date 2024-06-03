85158ddc1050eab094dec9714bcd2043d9c2726c
Danilo J. Palma <dan.palmav@duocuc.cl>
2024-06-03 17:04:16 -0400

    add routes, controllers and queries. Update license and readme files

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,20 @@
 import express from 'express';
-import { home } from '../controllers/controller.js';
+import { home, newUser, getUsers, updateUser, deleteUser, transfer, getTransactions} from '../controllers/controller.js';
 const router = express.Router();
 
 router.get('/', home);
 
+router.post('/usuario', newUser)
 
+router.get('/usuarios', getUsers)
+
+router.put('/usuario', updateUser)
+
+router.delete('/usuario', deleteUser)
+
+router.post('/transferencia', transfer)
+
+router.get('/transferencias', getTransactions)
 
 router.get('*', (req, res) => {
     res.status(404).send('Not Found');
